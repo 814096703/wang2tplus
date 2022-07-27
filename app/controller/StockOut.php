4204f35c46785c074798d9588dd32cdd96e7d3dd
chen <814096703@qq.com>
2022-07-27 09:43:57 +0800

    另建销售单接口

---
+++ app/controller/StockOut.php
@@ -0,0 +1,366 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+use think\facade\Db;
+use think\helper\Arr;
+
+include_once(__DIR__."/../api/wdtsdk.php");
+include_once(__DIR__."/WangApi.php");
+include_once(__DIR__."/../api/MyTplusSdk.php");
+include_once(__DIR__."/TplusApi.php");
+include_once(__DIR__."/TplusApi2.php");
+include_once(__DIR__."/TplusApi3.php");
+include_once(__DIR__."/../api/util.php");
+
+class StockOut extends BaseController{
+    public function test(){
+        $st = '2022-07-11 08:00:00';
+        $et = '2022-07-11 09:00:00';
+        $warehouse = '01';
+        $res = qmStockout(100, 1, ['start_time'=>$st, 'end_time'=>$et, 'status_type' => '3','status' => '110','warehouse_no'=>$warehouse]);
+        dump($res);
+    }
+}
+
+// 销售出库单
+function stockOut($st, $et, $warehouse){
+    $pagesize = 20;
+    $msg = '';
+    
+    $rangeTimeArrOut = getRangeTimeArr($st, $et);
+    foreach($rangeTimeArrOut as $rangeTimeOut){
+        if(strtotime($rangeTimeOut['start'])!=strtotime(date("Y-m-d"))){
+            $day_count = 0;
+            $orders = [];
+            // 获取一天24小时时间段
+            $rangeTimeArr = getDayHours($rangeTimeOut['start']);
+        
+            // 获取一天24小时的全部订单
+            foreach($rangeTimeArr as $rangeTime){
+                
+                $wangData = qmTest($rangeTime['start'], $rangeTime['end'], $pagesize, 1, $warehouse['wh_code']);
+                sleep(1);
+                if($wangData->status==0){
+                    
+                    $total = $wangData->data->total_count;
+                    $day_count+=$total;
+                    if($total>0){
+                        
+                        $pages = intval($total / $pagesize);
+                        if($total % $pagesize != 0){
+                            $pages += 1;
+                        }
+                        for($page=1; $page<=$pages; $page++){
+                            $pageData = qmTest($rangeTime['start'], $rangeTime['end'], $pagesize, $page, $warehouse['wh_code']);
+                            $orders = array_merge($orders, $pageData->data->order);
+                        }
+                    }
+                }
+            }
+            $msg=$msg.'销售出库'.date("Y-m-d", strtotime($rangeTimeOut['start'])).' 查询到'.$day_count."条数据".PHP_EOL;
+            $handmakeOrder = []; // 手工建单的订单
+            $apiOrder = []; // 接口抓取的订单
+        
+            // 检查订单是否已录入，并将订单分类
+            foreach($orders as $order){
+                // 查询单据是否已转到t+
+                $row = Db::table('fa_order')->where('order_num',$order->order_no)->find();
+                // $row = Db::table('voucher_log')->where('voucher_id',$order->order_no)->find();
+                if(!$row || $row['status']=='未同步'){
+                    // dump($order);
+                    // trade_from=2 手工建单 trade_from=3 导入
+                    // if($order->trade_from==2 || $order->trade_from==3){
+                    //     array_push($handmakeOrder, $order);
+                    // }
+                    if($order->trade_from==1){ // trade_from=1 API抓单 
+                        array_push($apiOrder, $order);
+                    }else{
+                        array_push($handmakeOrder, $order);
+                    }
+                }
+                
+                
+            }
+            // 手工建单 单张录入t+
+            if(count($handmakeOrder)>0){
+                
+                foreach($handmakeOrder as $order){
+                    // 单据录入到t+
+                    $res = w2tStockOut($order);
+
+                    $row = Db::table('fa_order')->where('order_num',$order->order_no)->find();
+                    if(!$row){
+                        $newRow = [
+                            'warehouse'=>$warehouse['wh_name'],
+                            'order_num'=>$order->order_no, 
+                            'order_detail'=>json_encode($order), 
+                            'order_time'=>strtotime($order->consign_time),
+                            'order_type'=>'销售出库单',
+                            'status'=>'未同步',
+                            'result'=>'未同步',
+                        ];
+                        
+                        if($res=='null'){
+                            // 单据信息录入到数据库
+                            $newRow['status'] = '已同步';
+                            $newRow['result'] = '已同步';
+                        }else{
+                            $msg.="录入失败：".$order->order_no.",".$res.PHP_EOL;
+                            $newRow['result'] = translateErrMsg(json_decode($res)->message);
+                            
+                        }
+                        Db::table('fa_order')->insert($newRow); 
+                    }else{
+                        
+                        if($res=='null'){
+                            // 单据信息录入到数据库
+                            $row['order_detail'] = json_encode($order);
+                            $row['status'] = '已同步';
+                            $row['result'] = '已同步';
+                        }else{
+                            $msg.="录入失败：".$order->order_no.",".$res.PHP_EOL;
+                            $row['result'] = translateErrMsg(json_decode($res)->message);
+                            
+                            
+                        }
+                        Db::table('fa_order')->update($row);
+                    }
+                     
+                }
+            }
+
+            // 对抓取的订单根据店铺进行细分
+            $shopUsed = [];
+            foreach($apiOrder as $order){
+                if(!in_array($order->shop_no, $shopUsed)){
+                    array_push($shopUsed, $order->shop_no);
+                }
+            }
+
+            $devideOrders = [];
+            
+            foreach($shopUsed as $shop){
+                $devideOrders[$shop] = [];
+                foreach($apiOrder as $order){
+                    if($order->shop_no==$shop){
+                        array_push($devideOrders[$shop], $order);
+                    }
+                }
+                
+            }
+            
+            // 接口抓取 合并录入t+
+            foreach($shopUsed as $shop){
+                $mixOrders = $devideOrders[$shop];
+                if(count($mixOrders)>0){
+                    
+                    $mixRes = w2tStockOutMany($mixOrders);
+                    
+                    if($mixRes=='null'){
+                        foreach($mixOrders as $order){
+                            $row = Db::table('fa_order')->where('order_num',$order->order_no)->find();
+                            if(!$row){
+                                $newRow = [
+                                    'warehouse'=>$warehouse['wh_name'],
+                                    'order_num'=>$order->order_no, 
+                                    'order_detail'=>json_encode($order), 
+                                    'order_time'=>strtotime($order->consign_time),
+                                    'order_type'=>'销售出库单',
+                                    'status'=>'已同步',
+                                    'result'=>'已同步',
+                                ];
+                                Db::table('fa_order')->insert($newRow);
+                            }else{
+                                $row['status'] = '已同步';
+                                $row['result'] = '已同步';
+                                Db::table('fa_order')->update($row);
+                            }
+                        }
+                    }else{
+                        $str = '';
+                        $msg.="合并录入失败：".$str.",".$mixRes.PHP_EOL;
+                        
+                        foreach($mixOrders as $order){
+                            $str.=$order->order_no.', ';
+                            $row = Db::table('fa_order')->where('order_num',$order->order_no)->find();
+                            $errMsg = translateErrMsg(json_decode($mixRes)->message);
+                            if(!$row){
+                                
+                                $newRow = [
+                                    'warehouse'=>$warehouse['wh_name'],
+                                    'order_num'=>$order->order_no, 
+                                    'order_detail'=>json_encode($order), 
+                                    'order_time'=>strtotime($order->consign_time),
+                                    'order_type'=>'销售出库单',
+                                    'status'=>'未同步',
+                                    'result'=>"合并录入失败,部分单据存在问题：".$errMsg
+                                ];
+                                Db::table('fa_order')->insert($newRow);
+                            }else{
+                                
+                                $row['result'] = "合并录入失败,部分单据存在问题：".$errMsg;
+                                Db::table('fa_order')->update($row);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            
+        }
+        sleep(1);
+    }
+    return $msg;
+    
+}
+
+// 销售出库wdt到t+ 单张录入
+function w2tStockOut($w_order){
+    $details = '';
+    foreach($w_order->details_list as $key=>$item){
+
+        $price = (floatval($item->cost_price)>=0.01 ? floatval($item->cost_price) : 0);
+        $amount =( floatval($item->total_amount)>=0.01 ? floatval($item->total_amount) : 0);
+        
+        $detail = '{
+            Inventory: {
+                Code: "'.$item->spec_no.'"
+            },
+            BaseQuantity: '.$item->goods_count.',
+            Price: '.$price.',
+            Amount: '.$amount.',
+            origTaxSalePrice: '.$item->sell_price.',
+            IsPresent: '.($item->gift_type?'true':'false').',
+            DynamicPropertyKeys: ["pubuserdefnvc1", "pubuserdefnvc2", "pubuserdefnvc3"],
+            DynamicPropertyValues: ["'.$w_order->src_trade_no.'", "'.$w_order->src_order_no.'", "'.$w_order->order_no.'"]
+        }';
+        if(count($w_order->details_list)==($key+1)){
+            $details.=$detail;
+        }else{
+            $details.=$detail.',';
+        }
+    }
+
+    $partner='';
+    $saleman='';
+    // 店铺名包含 '公司' ,客户网名作为客户，有业务员，不合并
+    // 网店，店铺作为客户，合并随意，没有业务员
+    if(strstr($w_order->shop_name, '公司')==false){
+        $partner=$w_order->shop_no;
+        
+    }else{
+        $partner=$w_order->customer_no;
+        $saleman = 'Clerk: {
+            Code: "'.$w_order->salesman_no.'"
+        },';
+    }
+     
+    $memo = $w_order->remark;
+    $memo = trim($memo, '"');
+    // 转化为t+数据格式
+    $content = '{
+        dto: {
+            IsModifiedCode:"true",
+            Code: "'.$w_order->order_no.'",
+            VoucherType: {
+                Code: "ST1021"
+            },
+            Partner: {
+                Code: "'.$partner.'"
+            },
+            VoucherDate: "'.date('Y-m-d',strtotime($w_order->consign_time)).'",
+            BusiType: {
+                Code: "15"
+            },
+            Warehouse: {
+                Code: "'.$w_order->warehouse_no.'"
+            },
+            '.$saleman.'
+            Memo: "'.$memo.'",
+            RDRecordDetails: [
+                '.$details.'
+            ]
+        }
+    }';
+
+    // dump($content);
+    $infoArr = getInfoArrByshop($w_order->shop_no);
+    // $res = saleDispatchCreate($infoArr['appKey'], $infoArr['appSecret'], $infoArr['token'], $content);
+    if(count($infoArr)>0){
+        $res = saleDispatchCreate($infoArr['appKey'], $infoArr['appSecret'], $infoArr['token'], $content);
+       
+    }else{
+        $res = '{"code":"EXERROR0001","message":"目标店铺'.$w_order->shop_no.'没有可执行账套","data":{"Code":"EXERROR0001","StatusCode":400,"islogerror":"1"}}';
+        // echo('res='.$res.'end');
+    }
+
+    return $res;
+}
+// 销售出库wdt到t+ 合并录入
+function w2tStockOutMany($orders){
+    
+    $details = '';
+    foreach($orders as $key=>$w_order){
+        foreach($w_order->details_list as $item){
+
+            $price = (floatval($item->cost_price)>=0.01 ? floatval($item->cost_price) : 0);
+            $amount =( floatval($item->total_amount)>=0.01 ? floatval($item->total_amount) : 0);
+            
+            $detail = '{
+                Inventory: {
+                    Code: "'.$item->spec_no.'"
+                },
+                BaseQuantity: '.$item->goods_count.',
+                Price: '.$price.',
+                Amount: '.$amount.',
+                DetailMemo: "'.$w_order->cs_remark.'",
+                origTaxSalePrice: '.$item->sell_price.',
+                IsPresent: '.($item->gift_type?'true':'false').',
+                DynamicPropertyKeys: ["pubuserdefnvc1", "pubuserdefnvc2", "pubuserdefnvc3"],
+                DynamicPropertyValues: ["'.$w_order->src_trade_no.'", "'.$w_order->src_order_no.'", "'.$w_order->order_no.'"]
+            }';
+            $details.=$detail.',';
+        }
+        if(count($orders)==($key+1)){
+            
+            $details = substr($details,0,strlen($details)-1);
+        }
+    }
+    
+    // 转化为t+数据格式
+    $content = '{
+        dto: {
+            IsModifiedCode:"false",
+            VoucherType: {
+                Code: "ST1021"
+            },
+            Partner: {
+                Code: "'.$orders[0]->shop_no.'"
+            },
+            VoucherDate: "'.date('Y-m-d',strtotime($orders[0]->consign_time)).'",
+            BusiType: {
+                Code: "15"
+            },
+            Warehouse: {
+                Code: "'.$orders[0]->warehouse_no.'"
+            },
+            RDRecordDetails: [
+                '.$details.'
+            ]
+        }
+    }';
+
+
+    $infoArr = getInfoArrByshop($orders[0]->shop_no);
+    
+    // $res = saleDispatchCreate($infoArr['appKey'], $infoArr['appSecret'], $infoArr['token'], $content);
+    if(count($infoArr)>0){
+        $res = saleDispatchCreate($infoArr['appKey'], $infoArr['appSecret'], $infoArr['token'], $content);
+    }else{
+        $res = '{"code":"EXERROR0001","message":"目标店铺'.$orders[0]->shop_no.'没有可执行账套","data":{"Code":"EXERROR0001","StatusCode":400,"islogerror":"1"}}';
+        // echo('res='.$res.'end');
+    }
+
+    return $res;
+}
---
+++ app/controller/WangApi.php
@@ -0,0 +1,336 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+include_once(__DIR__."/../api/wdtsdk.php");
+include_once(__DIR__."/../taobao-sdk-PHP-auto/TopSdk.php");
+include_once(__DIR__."/../taobao-sdk-PHP-auto/QimenCloud/Top/request/WdtWmsStockoutSalesQuerywithdetailRequest.php");
+include_once(__DIR__."/../taobao-sdk-PHP-auto/QimenCloud/top/request/WdtWmsStockinRefundQuerywithdetailRequest.php");
+include_once(__DIR__."/../taobao-sdk-PHP-auto/QimenCloud/top/request/WdtAftersalesRefundRefundSearchRequest.php");
+include_once(__DIR__."/../taobao-sdk-PHP-auto/WdtUtils.php");
+header('Content-type:text/html;charset=utf8');
+date_default_timezone_set('PRC');
+
+class WangApi extends BaseController
+{
+    public function index()
+    {
+        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:) </h1><p> ThinkPHP V' . \think\facade\App::version() . '<br/><span style="font-size:30px;">14载初心不改 - 你值得信赖的PHP框架</span></p><span style="font-size:25px;">[ V6.0 版本由 <a href="https://www.yisu.com/" target="yisu">亿速云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=64890268" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ee9b1aa918103c4fc"></think>';
+    }
+
+    public function test(){
+        // $res = qmTest('2022-06-07 09:00:00', '2022-06-07 10:00:00', 100, 1, '01');
+        // $res = stockinRefund('2022-06-01 09:00:00', '2022-06-02 10:00:00', 100, 1, '01');
+        $res = wangPurchaseReturnQueryWithDetail('2022-06-01 00:00:00', '2022-06-16 00:00:00', 10, 0, "01");
+        // $res = stockinPurchase('2022-02-01', '2022-02-23');
+        // $res = wangStockinTransfer('2022-06-01 00:00:00', '2022-06-06 00:00:00', 10, 0);
+        // $res = refundSearch('TK2206020021', 10, 1);
+        // echo json_encode($res);
+        dump($res);
+    }
+}
+
+// 采购入库单
+function stockinPurchase($start_time, $end_time, $pageSize, $pageNo, $warehouse){
+    
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));
+   
+    $pars = array
+    (
+        'start_time' => $start_time,
+        'end_time' => $end_time,
+        'warehouse_no'=>$warehouse,
+        'status'=>'80'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    return $response = $client->pageCall("wms.stockin.Purchase.queryWithDetail",$pager, $pars);
+}
+
+// 其他入库单（其他入库业务）
+function wangStockinOther($st, $et, $pageSize, $pageNo, $warehouse){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'start_time' => $st,
+        'end_time' => $et,
+        'warehouse_no'=>$warehouse,
+        'status'=>'80'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.stockin.Other.queryWithDetail",$pager, $pars);
+    
+    return $response;
+}
+
+// 调拨入库单
+function wangStockinTransfer($st, $et, $pageSize, $pageNo){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'start_time' => $st,
+        'end_time' => $et,
+        'status'=>'80'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.stockin.Transfer.queryWithDetail",$pager, $pars);
+    return $response;
+    // echo "<pre>";print_r($response);echo "<pre>";
+    // $php_json = json_encode($response);
+    // echo $php_json;
+}
+
+// 其他出库单
+function wangStockoutOther($st, $et, $pageSize, $pageNo, $warehouse){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'start_time' => $st,
+        'end_time' => $et,
+        'warehouse_no'=>$warehouse,
+        'status'=>'70'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.stockout.OtherQuery.queryWithDetail",$pager, $pars);
+    return $response;
+}
+
+// 调拨出库单
+function wangStockoutTransfer($st, $et, $pageSize, $pageNo, $warehouse){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'start_time' => $st,
+        'end_time' => $et,
+        'warehouse_no'=>$warehouse,
+        'status'=>'110'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.stockout.Transfer.queryWithDetail",$pager, $pars);
+    return $response;
+}
+
+// 销售出库单（奇门）
+function qmStockout($pageSize, $pageNo, $params){
+
+    $wdtAppKey = env('WANG.appkey');
+    $wdtAppSecret = env('WANG.appsecret');
+    $wdtSecretArr = explode(':', $wdtAppSecret);
+    $wdtSalt = $wdtSecretArr[1];
+    $wdtSecret = $wdtSecretArr[0];
+    $client = new \QimenCloudClient(env('WANG.qmAppkey'), '4bafe14675df13f72da15a96be473971');
+
+
+    // $client->gatewayUrl = 'http://3ldsmu02o9.api.taobao.com/router/qmtest';//测试
+    $client->gatewayUrl = 'http://3ldsmu02o9.api.taobao.com/router/qm';// 正式环境
+    $client->targetAppkey = '21363512';
+    $client->format = 'json';
+
+    $request = new \WdtWmsStockoutSalesQuerywithdetailRequest();
+
+    $timestamp = date("Y-m-d H:i:s");
+    // 'stockout_no' => 'CKDH20220209105',
+    // $params = array(
+    //     'start_time' => $st,
+    //     'end_time' => $et,
+    //     'status_type' => '3',
+    //     'status' => '110',
+    //     'warehouse_no'=>$warehouse,
+    //     // 'stockout_no'=> 'CK312635'
+    // );
+    
+    $pager = array(
+        'page_size' => $pageSize,
+        'page_no' => $pageNo
+    );
+    $request->setParams(json_encode($params));
+    $request->setPager(json_encode($pager));
+    $request->setDatetime($timestamp);
+    // dump($timestamp);
+    $request->setWdtAppkey($wdtAppKey);
+    $request->setWdtSalt($wdtSalt);
+    $request->putOtherTextParam("wdt3_customer_id", "xishuicun3");
+
+    $wdtUtils = new \WdtUtils();
+    $wdtSign = $wdtUtils->getQimenCustomWdtSign($request, $wdtSecret);
+    // dump($wdtSign);
+    $request->setWdtSign($wdtSign);
+
+    try {
+        $response = $client->execute($request);
+        // echo "\r\nresponse: " . json_encode($response) . "\n";
+        // dump($response);
+        return $response;
+    } catch (\Exception $e) {
+        echo "\r\nexception:" . $e->getMessage();
+        return (object)['status'=>1];
+    }
+}
+
+// 销退换货入库单（奇门）
+function stockinRefund($st, $et, $pageSize, $pageNo, $warehouse){
+
+    $wdtAppKey = env('WANG.appkey');
+    $wdtAppSecret = env('WANG.appsecret');
+    $wdtSecretArr = explode(':', $wdtAppSecret);
+    $wdtSalt = $wdtSecretArr[1];
+    $wdtSecret = $wdtSecretArr[0];
+    $client = new \QimenCloudClient(env('WANG.qmAppkey'), '4bafe14675df13f72da15a96be473971');
+
+
+    // $client->gatewayUrl = 'http://3ldsmu02o9.api.taobao.com/router/qmtest';//测试
+    $client->gatewayUrl = 'http://3ldsmu02o9.api.taobao.com/router/qm';// 正式环境
+    $client->targetAppkey = '21363512';
+    $client->format = 'json';
+
+    $request = new \WdtWmsStockinRefundQuerywithdetailRequest();
+
+    $timestamp = date("Y-m-d H:i:s");
+    // 'stockout_no' => 'CKDH20220209105',
+    $params = array(
+        'start_time' => $st,
+        'end_time' => $et,
+        'status' => '80',
+        'warehouse_no'=>$warehouse,
+        // 'stockout_no'=> 'CK312635'
+    );
+    
+    $pager = array(
+        'page_size' => $pageSize,
+        'page_no' => $pageNo
+    );
+    $request->setParams(json_encode($params));
+    $request->setPager(json_encode($pager));
+    $request->setDatetime($timestamp);
+    // dump($timestamp);
+    $request->setWdtAppkey($wdtAppKey);
+    $request->setWdtSalt($wdtSalt);
+    $request->putOtherTextParam("wdt3_customer_id", "xishuicun3");
+
+    $wdtUtils = new \WdtUtils();
+    $wdtSign = $wdtUtils->getQimenCustomWdtSign($request, $wdtSecret);
+    // dump($wdtSign);
+    $request->setWdtSign($wdtSign);
+
+    try {
+        $response = $client->execute($request);
+        // echo "\r\nresponse: " . json_encode($response) . "\n";
+        // dump($response);
+        return $response;
+    } catch (\Exception $e) {
+        echo "\r\nexception:" . $e->getMessage();
+        return (object)['status'=>1];
+    }
+}
+
+// 盘点入库单
+function wangQueryStockPdInDetail($st, $et, $pageSize, $pageNo, $warehouse){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'start_time' => $st,
+        'end_time' => $et,
+        'warehouse_no'=>$warehouse,
+        'status'=>'80'
+    );
+    // 'warehouse_no' => 'xishuicun3-test'
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.StockPd.queryStockPdInDetail",$pager, $pars);
+    return $response;
+}
+
+// 盘点出库单
+function wangQueryStockPdOutDetail($st, $et, $pageSize, $pageNo, $warehouse){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'start_time' => $st,
+        'end_time' => $et,
+        'warehouse_no'=>$warehouse,
+        'status'=>'110'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.StockPd.queryStockPdOutDetail",$pager, $pars);
+    return $response;
+}
+
+// 采购退货出库单
+function wangPurchaseReturnQueryWithDetail($st, $et, $pageSize, $pageNo, $warehouse){
+    $client = new \WdtErpClient(env('WANG.service_url'), env('WANG.sid'), env('WANG.appkey'), env('WANG.appsecret'));//直接输入ip参数
+    
+    $pars = array
+    (    
+        'out_start_time' => $st,
+        'out_end_time' => $et,
+        'warehouse_no'=>$warehouse,
+        'status'=>'110'
+    );
+    
+    $pager = new \Pager($pageSize, $pageNo, true);
+    $response = $client->pageCall("wms.stockout.PurchaseReturn.queryWithDetail",$pager, $pars);
+    return $response;
+}
+
+// 退换单查询（奇门）
+function refundSearch($refund_no, $pageSize, $pageNo){
+
+    $wdtAppKey = env('WANG.appkey');
+    $wdtAppSecret = env('WANG.appsecret');
+    $wdtSecretArr = explode(':', $wdtAppSecret);
+    $wdtSalt = $wdtSecretArr[1];
+    $wdtSecret = $wdtSecretArr[0];
+    $client = new \QimenCloudClient(env('WANG.qmAppkey'), '4bafe14675df13f72da15a96be473971');
+
+
+    // $client->gatewayUrl = 'http://3ldsmu02o9.api.taobao.com/router/qmtest';//测试
+    $client->gatewayUrl = 'http://3ldsmu02o9.api.taobao.com/router/qm';// 正式环境
+    $client->targetAppkey = '21363512';
+    $client->format = 'json';
+
+    $request = new \WdtAftersalesRefundRefundSearchRequest();
+
+    $timestamp = date("Y-m-d H:i:s");
+    // 'stockout_no' => 'CKDH20220209105',
+    $params = array(
+        "refund_no"=>$refund_no
+    );
+    
+    $pager = array(
+        'page_size' => $pageSize,
+        'page_no' => $pageNo
+    );
+    $request->setParams(json_encode($params));
+    $request->setPager(json_encode($pager));
+    $request->setDatetime($timestamp);
+    // dump($timestamp);
+    $request->setWdtAppkey($wdtAppKey);
+    $request->setWdtSalt($wdtSalt);
+    $request->putOtherTextParam("wdt3_customer_id", "xishuicun3");
+
+    $wdtUtils = new \WdtUtils();
+    $wdtSign = $wdtUtils->getQimenCustomWdtSign($request, $wdtSecret);
+    // dump($wdtSign);
+    $request->setWdtSign($wdtSign);
+
+    try {
+        $response = $client->execute($request);
+        // echo "\r\nresponse: " . json_encode($response) . "\n";
+        // dump($response);
+        return $response;
+    } catch (\Exception $e) {
+        echo "\r\nexception:" . $e->getMessage();
+        return (object)['status'=>1];
+    }
+}
